bb09bc35dd6a1edbda39c93d53169cd161cd6f39
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-09-26 23:06:18 -0500

    DB fisrts commit- DataBase modeling Pt 1

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type:DataTypes.INTEGER,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image:{
+      type:DataTypes.STRING,
+      allowNull: true,
+      defoult:'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg',
+    },
+    rating: {
+      type:DataTypes.FLOAT,
+    },
+    platforms:{
+    },
+    genres:{
+    },
+    released:{
+    },
+    //short_screenshots:{}
+    //tags:{}
+  });
+  sequelize.define('genres',{
+    id:{
+      type:DataTypes.STRING,
+      primaryKey: true,
+    },
+    name:{
+      type:DataTypes.STRING(30),
+    }
   });
 };
