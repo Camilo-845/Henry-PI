55531647ea51318b8cf6a9e13e4a98a62e70c9ee
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-10-01 12:38:33 -0500

    Db_Model_completed

---
+++ api/src/models/Genres.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  sequelize.define('genres',{
+    id:{
+      type:DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name:{
+      type:DataTypes.STRING(30),
+      allowNull: false,
+    }
+  });
+};
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -6,7 +6,12 @@ module.exports = (sequelize) => {
   sequelize.define('videogame', {
     id: {
       type:DataTypes.INTEGER,
-      primaryKey: true
+      primaryKey: true,
+      autoIncrement:true,
+    },
+    belongs_db:{
+      type:DataTypes.BOOLEAN,
+      defaultValue:true,
     },
     name: {
       type: DataTypes.STRING,
@@ -14,35 +19,24 @@ module.exports = (sequelize) => {
     },
     description:{
       type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image:{
-      type:DataTypes.STRING,
       allowNull: true,
-      default:'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg',
+      defaultValue:"Sin Descripcion"
     },
     rating: {
       type:DataTypes.FLOAT,
-    },
-    platforms:{
-    },
-    genres:{
+      allowNull:true,
     },
     released:{
       type: DataTypes.DATEONLY,
       allowNull:true,
       defaultValue: DataTypes.NOW,
     },
+    background_image:{
+      type:DataTypes.STRING,
+      allowNull:true,
+      defaultValue:'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg',
+    },
     //short_screenshots:{}
     //tags:{}
-  });
-  sequelize.define('genres',{
-    id:{
-      type:DataTypes.STRING,
-      primaryKey: true,
-    },
-    name:{
-      type:DataTypes.STRING(30),
-    }
   });
 };
