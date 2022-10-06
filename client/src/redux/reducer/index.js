8b06f5a2bba1e316138c1dc635cc2a15a01ea090
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-10-06 10:42:24 -0500

    Redux_reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,3 +1,4 @@
+import {GET_GENRES,GET_PLATFORMS,GET_VIDEOGAMES,GET_VIDEOGAME_BY_ID,CREATE_VIDEOGAME}from "../actions"
 const initialState = {
   videogames: [],
   videogameDetail: [],
@@ -7,7 +8,32 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    
+    case GET_VIDEOGAMES:
+      return {
+        ...state,
+        videogames:action.payload
+      }
+    case GET_VIDEOGAME_BY_ID:
+      return {
+        ...state,
+        videogameDetail:action.payload
+      }
+    case CREATE_VIDEOGAME:
+      return {
+        ...state,
+        videogames:[...state.videogames,action.payload]
+      }
+    case GET_GENRES:
+      return {
+        ...state,
+        geners:action.payload
+      }
+    case GET_PLATFORMS:
+      return {
+        ...state,
+        platforms:action.payload
+      }
+    default:return {...state}
   }
 };
 
