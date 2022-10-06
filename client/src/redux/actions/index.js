f3a7332416dbaa993ad9ee472532cc1614b55717
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-10-06 10:34:40 -0500

    Redux_actions

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -6,8 +6,55 @@ export const CREATE_VIDEOGAME = "CREATE_VIDEOGAME";
 export const GET_GENRES = "GET_GENRES";
 export const GET_PLATFORMS = "GET_PLATFORMS";
 
-export const getVideogames = () => {};
-export const getVideogameById = () => {};
-export const createVideogame = () => {};
-export const getGenres = () => {};
-export const getPlatforms = () => {};
+export const getVideogames = () => {
+    return function(dispatch){
+        return fetch(`${BACKEND_URL}videogames`)
+        .then((response)=>response.json())
+        .then(data=>{
+            dispatch({type:GET_VIDEOGAMES,payload:data})
+        })
+    }
+};
+export const getVideogameById = (IdVideogame,belongsDb) => {
+    var DbParams="";
+    if(belongsDb)DbParam+="?belongs_db=true"
+    return function(dispatch){
+        return fetch(`${BACKEND_URL}videogame/${IdVideogame}${DbParams}`)
+        .then(response=>response.json())
+        .then(data=>{
+            dispatch({type:GET_VIDEOGAME_BY_ID,payload:data})
+        })
+    }
+};
+export const createVideogame = (Videogame) => {
+    return {
+        type:CREATE_VIDEOGAME,
+        payload:{
+            name:Videogame.name,
+            description:Videogame.description,
+            rating:Videogame.rating,
+            realised:Videogame.realised,
+            background_image:Videogame.background_image,
+            genres:Videogame.genres,
+            platforms:Videogame.platforms,
+        }
+    }
+};
+export const getGenres = () => {
+    return function(dispatch){
+        return fetch(`${BACKEND_URL}genres`)
+        .then((response)=>response.json())
+        .then(data=>{
+            dispatch({type:GET_GENRES,payload:data})
+        })
+    }
+};
+export const getPlatforms = () => {
+    return function(dispatch){
+        return fetch(`${BACKEND_URL}platforms`)
+        .then((response)=>response.json())
+        .then(data=>{
+            dispatch({type:GET_PLATFORMS,payload:data})
+        })
+    }
+};
