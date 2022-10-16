f6797fec80ccc9653194507ef96f17c17aed9131
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-10-16 18:14:31 -0500

    deploy configuration_2

--- api/index.js
+++ api/index.js
@@ -20,14 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
-const {
-  PORT
-} = process.env;
+const {PORT} = process.env;
 
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log('%s listening at ',PORT); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
