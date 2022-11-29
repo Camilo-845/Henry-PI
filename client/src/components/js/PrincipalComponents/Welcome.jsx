6e7664a3daaec75f04aeadab5fa47af6a7124663
Camilo S <camilo.sarmiento.amado@gmail.com>
2022-11-28 22:05:16 -0500

    cambios temÃporales PI

--- client/src/components/js/PrincipalComponents/Welcome.jsx
+++ client/src/components/js/PrincipalComponents/Welcome.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 const Welcome = () => {
   return (
     <div className={styles.main_container}>
-      <img  src={require("../../../scr_img/Control_2.png").default} alt="hola"/>
       <Link to={`/videogame`} >
       <img className={styles.image_2} src={require("../../../scr_img/Vanilla-1s-286px.svg").default} alt="hola"/>
       </Link>
